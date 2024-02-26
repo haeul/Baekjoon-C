24346c0943daa91a97a38e0b266f3f669aec4f0d
haeul <50603554+haeul@users.noreply.github.com>
2024-02-26 23:52:29 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char s1[51];
+	scanf("%s", s1);
+	printf("%s??!", s1);
+	
+	return 0;
+}
