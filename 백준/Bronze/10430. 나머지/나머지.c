76c96a752f6627d095d192bf510e92c2911c56c0
haeul <50603554+haeul@users.noreply.github.com>
2024-02-26 23:56:42 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d\n%d\n%d\n%d", (a+b)%c, ((a%c)+(b%c))%c, (a*b)%c, ((a%c)*(b%c))%c);
+	
+	return 0;
+}
