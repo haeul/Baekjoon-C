2d96e0734122c5a61ab988bb1474a89da9aa9946
haeul <50603554+haeul@users.noreply.github.com>
2024-02-28 08:47:35 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+    int score;
+
+    scanf("%d", &score);
+
+    if (score >= 90)
+        printf("A");
+    else if (score >= 80)
+        printf("B");
+    else if (score >= 70)
+        printf("C");
+    else if (score >= 60)
+        printf("D");
+    else
+        printf("F");
+ 
+    return 0;
+}
