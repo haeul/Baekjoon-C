c6637c2463bd7167d5c44514772b83bcaa34e47d
haeul <50603554+haeul@users.noreply.github.com>
2024-04-11 11:45:38 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    printf("%d\n1", n);
+
+    return 0;
+}
