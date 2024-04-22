be79c35e0970461b339d99e196873c55e05ad07e
haeul <50603554+haeul@users.noreply.github.com>
2024-04-22 18:51:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 68 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+typedef struct {
+    int x;
+    int y;
+}Point;
+
+int compare(const void* a, const void* b) {
+    Point num1 = *(Point*)a;
+    Point num2 = *(Point*)b;
+    if (num1.x < num2.x)
+        return -1;
+    if (num1.x > num2.x)
+        return 1;
+    else if (num1.x == num2.x) {
+        if (num1.y > num2.y)
+            return 1;
+        else
+            return -1;
+    }
+}
+
+int main() {
+    int N = 0;
+
+    scanf("%d", &N);
+    Point* arr = (Point*)malloc(sizeof(Point) * N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+    }
+    qsort(arr, N, sizeof(Point), compare);
+
+    for (int i = 0; i < N; i++) {
+        printf("%d %d\n", arr[i].x, arr[i].y);
+    }
+
+    return 0;
+}
